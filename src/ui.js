22fdff63b9bec74840f860dd24143c61a51baea1
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-03-02 17:38:37 -0800

    Posts can now be added, also added an alert for that

--- src/ui.js
+++ src/ui.js
@@ -30,6 +30,41 @@ class UI {
 
         this.post.innerHTML = output;
     }
+
+    showAlert(message, className) {
+        this.clearAlert();
+
+        // Create div
+        const div = document.createElement('div');
+        // Add classes
+        div.className = className;
+        // Add text
+        div.appendChild(document.createTextNode(message));
+        //Get parent
+        const container = document.querySelector('.postsContainer');
+        // Get posts
+        const posts = document.querySelector('#posts');
+        // Insert alert div
+        container.insertBefore(div, posts);
+
+        // Timeout
+        setTimeout(() => {
+            this.clearAlert();
+        }, 2000)
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert');
+
+        if(currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput = document.querySelector('#title').value = '';
+        this.bodyInput = document.querySelector('#body').value = '';
+    }
 }
 
 export const ui = new UI();
